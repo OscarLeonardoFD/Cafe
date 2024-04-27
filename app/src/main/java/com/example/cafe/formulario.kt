db43a05d266b02a95878bbdcbaf34b6ba3c7a2ff
OscarLeonardoFD <oscar.fonsecad@uniagustiniana.edu.co>
2024-04-27 07:33:06 -0500

    2commit

--- app/src/main/java/com/example/cafe/formulario.kt
+++ app/src/main/java/com/example/cafe/formulario.kt
@@ -1,24 +1,74 @@
 package com.example.cafe
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.firestore.FirebaseFirestore
+
+
 
 class formulario : AppCompatActivity() {
+
+    val db = FirebaseFirestore.getInstance()
+    private lateinit var formulario : formulario
+    val button = findViewById<Button>(R.id.bcrear)
+    val editnombre = findViewById<EditText>(R.id.editnombre)
+    val editcontraseña = findViewById<EditText>(R.id.editcontraseña)
+    val edittelefono = findViewById<EditText>(R.id.edittelefono)
+    val editcorreo = findViewById<EditText>(R.id.editcorreo)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        formulario = formulario.inflate(layoutInflater)
+
         setContentView(R.layout.activity_formulario)
-        val edittext = findViewById<EditText>(R.id.editnombre)
-        val edittext2 = findViewById<EditText>(R.id.editcontraseña)
-        val edittext3 = findViewById<EditText>(R.id.edittelefono)
-        val edittext4 = findViewById<EditText>(R.id.editcorreo)
-        val button = findViewById<Button>(R.id.bcrear)
-        button.setOnClickListener {
+
+
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message","comenzando app")
+        analytics.logEvent("formulario",bundle)
+
+       formulario.button.setOnClickListener {
+           val user = hashMapOf(
+           "first" to formulario.editnombre.text.toString(),
+           "last" to formulario.editcontraseña.text.toString(),
+               "last" to formulario.edittelefono.text.toString(),
+           "last" to formulario.editcorreo.text.toString()
+
+           )
+           db.collection("usuarios")
+               .add(user)
+               .addOnSuccessListener { documentReference ->
+                   Log.d("TAG",documentReference.id )
+
+               }
+               .addOnFailureListener{e->
+                   Log.w("TAG", "error $e")
+
+               }
+
+
+       }
+
 
 
         }
 
+
+
+
+
+
     }
-}
+
+
+
+
+
+
+
--- app/src/main/java/com/example/cafe/login.kt
+++ app/src/main/java/com/example/cafe/login.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 
+
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
