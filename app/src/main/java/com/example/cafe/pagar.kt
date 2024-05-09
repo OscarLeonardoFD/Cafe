71eb6e7c542ea759aa10c15bb913ed4f9a4a823e
OscarLeonardoFD <oscar.fonsecad@uniagustiniana.edu.co>
2024-05-09 18:51:48 -0500

    4commit

---
+++ app/src/main/java/com/example/cafe/pagar.kt
@@ -0,0 +1,20 @@
+package com.example.cafe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class pagar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_total)
+        val texttotal = findViewById<TextView>(R.id.texttotal)
+        //recibirlo del otro activity
+        val totalAmount = intent.getDoubleExtra("TOTAL_AMOUNT", 0.0)
+        texttotal.text ="Total: $totalAmount"
+
+
+
+
+    }
+}
